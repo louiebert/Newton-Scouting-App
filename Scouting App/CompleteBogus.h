1637a24d12efde35f62274c94185370ca8e2f55f
Louie Bertoncin <louiebertoncin@gmail.com>
2013-10-30 21:21:54 -0500

    Added bogus file

---
+++ Scouting App/CompleteBogus.h
@@ -0,0 +1,13 @@
+//
+//  CompleteBogus.h
+//  NewtN Scouting
+//
+//  Created by Eris on 10/30/13.
+//  Copyright (c) 2013 Louie Bertoncin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CompleteBogus : NSObject
+
+@end
---
+++ Scouting App/CompleteBogus.m
@@ -0,0 +1,13 @@
+//
+//  CompleteBogus.m
+//  NewtN Scouting
+//
+//  Created by Eris on 10/30/13.
+//  Copyright (c) 2013 Louie Bertoncin. All rights reserved.
+//
+
+#import "CompleteBogus.h"
+
+@implementation CompleteBogus
+
+@end
